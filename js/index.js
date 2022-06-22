a46e9d2fe32c650f478b5d2add864b2f17232b85
coditor-india <atcozoneus@gmail.com>
2022-06-22 23:13:23 +0530

    js day 1

---
+++ js/index.js
@@ -0,0 +1,18 @@
+evenOfOdd(15)
+evenOfOdd(78);
+
+function evenOfOdd(num) {
+    if (num % 2 == 0) {
+        console.log(num + " is even");
+    } else {
+        console.log(num + " is odd");
+    }
+}
+
+
+
+Calculator
+--add
+--subtract
+--multiply
+--divide
